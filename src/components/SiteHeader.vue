51f59a2f5e8ee4d69f009b89a4ac79466f03c682
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-18 11:50:26 +0100

    axios call and stamp result

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,31 +1,65 @@
 <template>
   <header>
     <SearchMovies @search-movies="search" />
-    <div></div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="movie" v-for="movie in movies" :key="movie.id">
+            <div class="title">{{ movie.title }}</div>
+            <div class="original-title">{{ movie.original_title }}</div>
+            <div class="language">{{ movie.original_language }}</div>
+            <div class="vote">{{ movie.vote_average }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
-// import axios from "axios";
+import axios from "axios";
 import SearchMovies from "./SearchMovies.vue";
 export default {
+  mounted() {
+    setTimeout(this.callApi, 1 * 1000);
+  },
+
   data() {
     return {
       searchSelected: "",
+      movies: [],
     };
   },
   components: {
     SearchMovies,
   },
+
   methods: {
     search(movie) {
       this.searchSelected = movie;
     },
-  },
 
-  computed: {},
+    callApi() {
+      var config = {
+        method: "get",
+        url: "https://api.themoviedb.org/3/search/movie?api_key=33e1e99b35059079af3232f95a0930b3&query=ritorno+al+futuro",
+      };
+
+      axios(config)
+        .then((response) => {
+          // console.log(response.data.results);
+          this.movies = response.data.results;
+          console.log(this.movies);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 
 <style>
+.movie {
+}
 </style>
